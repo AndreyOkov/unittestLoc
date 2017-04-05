0afe7d248ca674b998f5c68df85785d84a046479
Andrey Ovchinnikov <kukuruza-11@yandex.ru>
2017-04-05 14:06:25 +0300

    Денис Елисеев Создание своего тестировщика Тестирование с помощью unittest

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'sqlite:E:/OpenServer/domains/unittest.loc/data/base.db',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'enableSchemaCache' => true // отключает вывод запросов в debuger
+
+];
